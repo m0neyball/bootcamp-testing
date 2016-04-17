e13518c59301c3d39ab158bfd4fc8ee91963a948
fox <fox@moneyball.io>
2016-04-17 17:04:10 +0800

    Add get name and cost method

--- app/Product.php
+++ app/Product.php
@@ -1,3 +1,36 @@
 <?php
+namespace App;
 
-class Product {}
+class Product
+{
+    protected $name;
+
+    protected $cost;
+
+    /**
+     * Product constructor.
+     *
+     * @param $name
+     */
+    public function __construct ($name, $cost)
+    {
+        $this->name = $name;
+        $this->cost = $cost;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName ()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCost ()
+    {
+        return $this->cost;
+    }
+}
