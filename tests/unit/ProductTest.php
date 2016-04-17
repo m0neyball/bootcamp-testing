3702d6dc545833a9a855bdc73eb6d103a56089e4
fox <fox@moneyball.io>
2016-04-17 16:57:49 +0800

    Test a prodcut has cost

--- tests/unit/ProductTest.php
+++ tests/unit/ProductTest.php
@@ -9,4 +9,11 @@ class ProductTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals('Fallout 4', $product->getName());
     }
+
+    public function testAProductHasCost ()
+    {
+        $product = new Product('Fallout 4', 59);
+
+        $this->assertEquals(59, $product->getCost());
+    }
 }
