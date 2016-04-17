4d3cf2c5f93754cc7f9febda3c651662289fb026
fox <fox@moneyball.io>
2016-04-17 21:41:32 +0800

    Test an order can determine the total cost of all its products

--- tests/unit/OrderTest.php
+++ tests/unit/OrderTest.php
@@ -18,6 +18,21 @@ class OrderTest extends PHPUnit_Framework_TestCase
         $order->add($product2);
 
         $this->assertCount(2, $order->products());
-        //$this->assertEquals(2, count($order->products()));
+    }
+
+    /**
+     * @test an order can determine the_total cost of all its products
+     */
+    public function an_order_can_determine_the_total_cost_of_all_its_products ()
+    {
+        $order = new Order();
+
+        $product = new Product('Fallout 4', 59);
+        $product2 = new Product('Pillowcase', 7);
+
+        $order->add($product);
+        $order->add($product2);
+
+        $this->assertEquals(66, $order->total());
     }
 }
