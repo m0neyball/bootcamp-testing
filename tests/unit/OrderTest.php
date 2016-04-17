01d390bdb844dddecba1d24668a4d3f4fe7b3a5a
fox <fox@moneyball.io>
2016-04-17 22:38:40 +0800

    Refractory order test cast use set up

--- tests/unit/OrderTest.php
+++ tests/unit/OrderTest.php
@@ -4,20 +4,22 @@ use App\Product;
 
 class OrderTest extends PHPUnit_Framework_TestCase
 {
+    protected $order;
+
+    public function setUp ()
+    {
+        $this->order = new Order();
+        $product = new Product('Fallout 4', 59);
+        $product2 = new Product('Pillowcase', 7);
+        $this->order->add($product);
+        $this->order->add($product2);
+    }
     /**
      * @test order consists of products
      */
     public function an_order_consists_of_products ()
     {
-        $order = new Order();
-
-        $product = new Product('Fallout 4', 59);
-        $product2 = new Product('Pillowcase', 7);
-
-        $order->add($product);
-        $order->add($product2);
-
-        $this->assertCount(2, $order->products());
+        $this->assertCount(2, $this->order->products());
     }
 
     /**
@@ -25,14 +27,7 @@ class OrderTest extends PHPUnit_Framework_TestCase
      */
     public function an_order_can_determine_the_total_cost_of_all_its_products ()
     {
-        $order = new Order();
-
-        $product = new Product('Fallout 4', 59);
-        $product2 = new Product('Pillowcase', 7);
-
-        $order->add($product);
-        $order->add($product2);
-
-        $this->assertEquals(66, $order->total());
+        $this->assertEquals(66, $this->order->total());
     }
+
 }
