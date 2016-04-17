5321bc59ce1591a4f51c8f60b160512f8809d632
fox <fox@moneyball.io>
2016-04-17 15:01:37 +0800

    Add first test case - video01

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -14,8 +14,15 @@ class ExampleTest extends TestCase
     public function testBasicExample()
     {
         // 1. Visit the home page
+        $this->visit('/')
+             ->see('Click Me');
+
         // 2. Press a "Click Me" link
         // 3. Sen "You've been clicked, punk."
+        $this->click('Click Me')
+             ->see("You've been clicked, punk.");
+
         // 4. Assert than the current url is /feedback
+        $this->seePageIs('/feedback');
     }
 }
