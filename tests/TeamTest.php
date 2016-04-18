4aae365673d0b2198a700503aff45d7e38491176
fox <fox@moneyball.io>
2016-04-18 23:05:26 +0800

    Test case for team crud

--- tests/TeamTest.php
+++ tests/TeamTest.php
@@ -33,4 +33,55 @@ class TeamTest extends TestCase
 
         $this->assertEquals(2, $team->count());
     }
+
+    /**
+     * @test a team can add multiple members at once
+     */
+    public function a_team_can_add_multiple_members_at_once ()
+    {
+        $team = factory(Team::class)->create();
+
+        $user = factory(User::class, 2)->create();
+
+        $team->add($user);
+
+        $this->assertEquals(2, $team->count());
+    }
+
+    /**
+     * @test a team has a maximum size
+     */
+    public function a_team_has_a_maximum_size ()
+    {
+        $team = factory(Team::class)->create(['size' => 2]);
+
+        $userOne = factory(User::class)->create();
+        $userTwo = factory(User::class)->create();
+
+        $team->add($userOne);
+        $team->add($userTwo);
+
+        $this->assertEquals(2, $team->count());
+
+        $this->setExpectedException('Exception');
+
+        $userThree = factory(User::class)->create();
+        $team->add($userThree);
+    }
+
+    /**
+     * @test a team can remove a member
+     */
+    public function a_team_can_remove_a_member ()
+    {
+
+    }
+
+    /**
+     * @test a team can remove all members at once
+     */
+    public function a_team_can_remove_all_members_at_once ()
+    {
+
+    }
 }
