6bb47227e6b7fb3d5fcef3948747d006bad400e2
ben <ben@moneyball.io>
2016-04-18 22:41:17 +0800

    Test an order consists of products

--- tests/unit/OrderTest.php
+++ tests/unit/OrderTest.php
@@ -1,6 +1,19 @@
 <?php
+use App\Product;
+use App\Order;
 
 class OrderTest extends TestCase
 {
-
+    /**
+     * @test
+     */
+    public function an_order_consists_of_products ()
+    {
+        $order = new Order();
+        $product1 = new Product('Fallout 4', 59);
+        $product2 = new Product('Pillowcase', 7);
+        $order->add ($product1);
+        $order->add ($product2);
+        $this->assertEquals (2, $order->products ());
+    }
 }
