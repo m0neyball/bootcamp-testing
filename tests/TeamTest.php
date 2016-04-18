e890b4c11c3ea030d74070d70a4e514e4ac5b399
fox <fox@moneyball.io>
2016-04-18 13:20:30 +0800

    Test a team has name

---
+++ tests/TeamTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class TeamTest extends TestCase
+{
+    /**
+     * @test a team has name
+     */
+    public function a_team_has_name ()
+    {
+        $team = new Team(['name' => 'Acme']);
+
+        $this->assertEquals('Acme', $team->name);
+    }
+}
