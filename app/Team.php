282f827956339c99160e3fdd3be7778cdff8c735
fox <fox@moneyball.io>
2016-04-18 18:01:52 +0800

    Add team members relationship and count

--- app/Team.php
+++ app/Team.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'size'];
+
+    public function add ($user)
+    {
+        $this->members()->save($user);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function members ()
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function count ()
+    {
+        return $this->members()->count();
+    }
 }
