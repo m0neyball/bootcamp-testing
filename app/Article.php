7232aba49e93476272c552e214801421fdecb0f7
fox <fox@moneyball.io>
2016-04-18 23:07:45 +0800

    Add article ide hepler annotation

--- app/Article.php
+++ app/Article.php
@@ -8,6 +8,17 @@ use Illuminate\Database\Eloquent\Model;
  * App\Article
  *
  * @mixin \Eloquent
+ * @property integer $id
+ * @property string $title
+ * @property integer $reads
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereTitle($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereReads($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article trending($take = 3)
  */
 class Article extends Model
 {
