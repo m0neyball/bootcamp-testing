4a7908967f8067579260ce6464067fdf48473e48
ben <ben@moneyball.io>
2016-04-19 17:36:00 +0800

    Refractory Order class total method

--- app/Order.php
+++ app/Order.php
@@ -17,11 +17,16 @@ class Order
 
     public function total ()
     {
+        /*
         $total = 0;
         foreach ($this->products () as $product) {
             $total += $product->cost ();
         }
 
         return $total;
+        */
+        return array_reduce ($this->products, function ($carry, $product) {
+            return $carry + $product->cost ();
+        });
     }
 }
