082d84b3a4e3ce7019fc84d14e4a9f966c6d41ac
lyndon <lyndon@moneyball.io>
2016-04-20 13:41:10 +0800

    Test Order

---
+++ app/Order.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2016/4/20
+ * Time: 下午1:39
+ */
+
+namespace App;
+
+
+class Order
+{
+
+}
---
+++ tests/unit/OrderTest.php
@@ -0,0 +1,28 @@
+<?php
+use App\Product;
+use App\Order;
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2016/4/20
+ * Time: 下午1:18
+ */
+
+
+
+class OrderTest extends PHPUnit_Framework_TestCase
+{
+    /** @test */
+    function an_order_consists_of_products()
+    {
+        $order =new Order;
+
+        $product = new Product('Fallout 4', 59);
+        $product2 = new Product('Pillowcase', 7);
+
+        $order->add($product);
+        $order->add($product2);
+
+        $this->assertEquals(2, count($order->prouducts()));
+    }
+}
