c6d6a5dff1f06bc4a6bc242b534347aa5371f68a
ben <ben@moneyball.io>
2016-04-20 14:03:49 +0800

    Test it fetches trending articles

--- tests/integration/models/ArticleTest.php
+++ tests/integration/models/ArticleTest.php
@@ -6,5 +6,19 @@ class ArticleTest extends TestCase
 {
     use DatabaseTransactions;
 
-
+    /**
+     * @test
+     */
+    public function it_fetches_trending_articles ()
+    {
+        // Given
+        factory (Article::class, 2)->create ();
+        factory (Article::class)->create (['reads' => 10]);
+        $mostPopular = factory (Article::class)->create (['reads' => 20]);
+        // When
+        $articles = Article::trending ();
+        // Then
+        $this->assertEquals ($mostPopular->id, $articles->first ()->id);
+        $this->assertCount(3, $articles);
+    }
 }
