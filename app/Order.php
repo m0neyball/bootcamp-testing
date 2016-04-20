011a0213c2c7e84f3ba8938b6f2796076e53e915
lyndon <lyndon@moneyball.io>
2016-04-20 14:22:31 +0800

    Refractory Order cost total

--- app/Order.php
+++ app/Order.php
@@ -25,12 +25,8 @@ class Order
 
     public function total()
     {
-        $total = 0;
-
-        foreach ($this->products as $product) {
-            $total += $product->cost();
-        }
-
-        return $total;
+       return array_reduce($this->products, function($carry, $product){
+           return $carry + $product->cost();
+       });
     }
 }
