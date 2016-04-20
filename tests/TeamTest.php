887a3b7bf240489b9bd30f242935456a8bd212a8
lyndon <lyndon@moneyball.io>
2016-04-20 16:57:03 +0800

    Add team_can_member case in TeamTest

--- tests/TeamTest.php
+++ tests/TeamTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Team;
+use App\User;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
@@ -11,8 +13,20 @@ class TeamTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+    /** @test */
+    function a_team_has_a_name()
     {
-        $this->assertTrue(true);
+        $team = new Team(['name' => 'Acme']);
+
+        $this->assertEquals('Acme', $team->name);
+    }
+    
+
+    /** @test */
+    function a_team_can_add_members()
+    {
+        $team = factory(Team::class)->create();
+
+        $user = factory(User::class)->create();
     }
 }
