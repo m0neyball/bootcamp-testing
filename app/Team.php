84517a23aca17952e3b6a9525952da4856971580
ben <ben@moneyball.io>
2016-04-20 20:29:02 +0800

    Add Team model add, mebers, count methods and size

--- app/Team.php
+++ app/Team.php
@@ -5,5 +5,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'size'];
+
+    public function add (User $user)
+    {
+        $this->members ()->save ($user);
+    }
+
+    public function members ()
+    {
+        return $this->hasMany (User::class);
+    }
+
+    public function count ()
+    {
+        return $this->members()->count();
+    }
 }
