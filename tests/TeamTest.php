a62181670289f1e8bebe1623b9e5dd909193ab7d
ben <ben@moneyball.io>
2016-04-20 20:23:12 +0800

    Test a team can add members form TeamTest

--- tests/TeamTest.php
+++ tests/TeamTest.php
@@ -3,9 +3,12 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\Team;
+use App\User;
 
 class TeamTest extends TestCase
 {
+    use DatabaseTransactions;
+
     /**
      * @test
      */
@@ -13,5 +16,18 @@ class TeamTest extends TestCase
     {
         $team = new Team(['name' => 'Acme']);
         $this->assertEquals ('Acme', $team->name);
+    }
+
+    /**
+     * @test
+     */
+    public function a_team_can_add_members ()
+    {
+        $team = factory (Team::class)->create ();
+        $user1 = factory (User::class)->create ();
+        $user2 = factory (User::class)->create ();
+        $team->add ($user1);
+        $team->add ($user2);
+        $this->assertEquals (2, $team->count ());
     }
 }
