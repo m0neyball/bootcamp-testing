f15edfc021bf7a4e8580950255aa589866112457
lyndon <lyndon@moneyball.io>
2016-04-20 14:18:45 +0800

    Test Order cost total

--- tests/unit/OrderTest.php
+++ tests/unit/OrderTest.php
@@ -25,4 +25,18 @@ class OrderTest extends PHPUnit_Framework_TestCase
 
         $this->assertCount(2, $order->products());
     }
+    /** @test */
+    function an_order_can_determine_the_total_cost_of_all_its_products()
+    {
+        $order =new Order;
+
+        $product = new Product('Fallout 4', 59);
+        $product2 = new Product('Pillowcase', 7);
+
+        $order->add($product);
+        $order->add($product2);
+
+        $this->assertEquals(66, $order->total());
+
+    }
 }
