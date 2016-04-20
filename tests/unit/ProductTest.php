d8f228eee4871325cd15910696fc968b570842b6
lyndon <lyndon@moneyball.io>
2016-04-20 11:45:53 +0800

    Test Product

--- tests/unit/ProductTest.php
+++ tests/unit/ProductTest.php
@@ -9,17 +9,24 @@ use App\Product;
 
 class ProductTest extends PHPUnit_Framework_TestCase
 {
+    protected $product;
+
+    public function setUp()
+    {
+        $this->product = new Product('Fallout 4', 59);
+    }
+
     function testAProductHasName()
     {
-        $product = new Product('Fallout 4', 59);
+//        $product = new Product('Fallout 4', 59);
 
-        $this->assertEquals('Fallout 4', $product->name());
+        $this->assertEquals('Fallout 4', $this->product->name());
     }
 
     function testAProductHasCose()
     {
-        $product = new Product('Fallout 4', 59);
+//        $product = new Product('Fallout 4', 59);
 
-        $this->assertEquals('59', $product->cost());
+        $this->assertEquals('59', $this->product->cost());
     }
 }
