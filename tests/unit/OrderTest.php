0514e67274ebda0be468fa4c5347bc12b0ab2600
lyndon <lyndon@moneyball.io>
2016-04-20 14:27:18 +0800

    Refractory OrderTest createOrderwithProducts

--- tests/unit/OrderTest.php
+++ tests/unit/OrderTest.php
@@ -15,18 +15,20 @@ class OrderTest extends PHPUnit_Framework_TestCase
     /** @test */
     function an_order_consists_of_products()
     {
-        $order =new Order;
-
-        $product = new Product('Fallout 4', 59);
-        $product2 = new Product('Pillowcase', 7);
-
-        $order->add($product);
-        $order->add($product2);
+        $order = $this->createOrderWithProducts();
 
         $this->assertCount(2, $order->products());
     }
     /** @test */
     function an_order_can_determine_the_total_cost_of_all_its_products()
+    {
+        $order = $this->createOrderWithProducts();
+
+        $this->assertEquals(66, $order->total());
+
+    }
+
+    protected function createOrderWithProducts()
     {
         $order =new Order;
 
@@ -36,7 +38,6 @@ class OrderTest extends PHPUnit_Framework_TestCase
         $order->add($product);
         $order->add($product2);
 
-        $this->assertEquals(66, $order->total());
-
+        return $order;
     }
 }
