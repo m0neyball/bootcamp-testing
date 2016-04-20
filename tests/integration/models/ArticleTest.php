1894e5d11c256119e1cdfd37c570c839472d7d81
lyndon <lyndon@moneyball.io>
2016-04-20 15:41:17 +0800

    Add trending case in ArticleTest

--- tests/integration/models/ArticleTest.php
+++ tests/integration/models/ArticleTest.php
@@ -6,7 +6,13 @@
  * Date: 2016/4/20
  * Time: 下午3:20
  */
-class ArticleTest
+class ArticleTest extends TestCase
 {
-
+    /** @test */
+    function it_fetches_trending_articles()
+    {
+        //Given
+        //When
+        //Then
+    }
 }
