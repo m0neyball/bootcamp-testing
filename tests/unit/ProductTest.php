6637f16c320728ab5421ebe29f7a5cad2ee14015
lyndon <lyndon@moneyball.io>
2016-04-20 11:39:16 +0800

    Add ProductTest.php

---
+++ tests/unit/ProductTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2016/4/20
+ * Time: 上午10:28
+ */
+use App\Product;
+
+class ProductTest extends PHPUnit_Framework_TestCase
+{
+    function testAProductHasName()
+    {
+        $product = new Product('Fallout 4', 59);
+
+        $this->assertEquals('Fallout 4', $product->name());
+    }
+
+    function testAProductHasCose()
+    {
+        $product = new Product('Fallout 4', 59);
+
+        $this->assertEquals('59', $product->cost());
+    }
+}
