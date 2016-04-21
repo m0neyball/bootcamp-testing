85ca325c3eb68f2a415449ff2cea30524fa9e823
lyndon <lyndon@moneyball.io>
2016-04-21 14:41:33 +0800

    Add LikesTest

---
+++ tests/integration/LikesTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2016/4/21
+ * Time: 下午2:36
+ */
+
+class LikesTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function a_user_can_like_a_post()
+    {
+        //given I have a post
+        //and a user
+        //and that user is logged in
+
+        //when they like a post
+
+        //then we should see evidence in database, and the post should be liked.
+        
+
+    }
+}
