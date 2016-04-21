f87c61be7452595be1a04874e7c8322b9772a558
lyndon <lyndon@moneyball.io>
2016-04-21 15:48:30 +0800

    Add isLiked method in Post model

--- app/Post.php
+++ app/Post.php
@@ -2,12 +2,30 @@
 
 namespace App;
 
+use Auth;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    /**
+     * like
+     */
     public function like()
     {
-        
+        $like = new Like(['user_id' => Auth::id()]);
+
+        $this->likes()->save($like);
+    }
+
+    public function likes()
+    {
+        return $this->morphMany(Like::class, 'likeable');
+    }
+
+    public function isLiked()
+    {
+        return !! $this->likes()
+                    ->where('user_id', Auth::id())
+                    ->count();
     }
 }
