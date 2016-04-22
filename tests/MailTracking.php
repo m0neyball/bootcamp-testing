4746adfaad7d7e51b733b4ed932521b1e94c36d5
lyndon <lyndon@moneyball.io>
2016-04-22 15:56:38 +0800

    Add feature to MailTracking trait and move TestingMailEventListener class to MailTracking class

--- tests/MailTracking.php
+++ tests/MailTracking.php
@@ -8,5 +8,51 @@
 
 trait MailTracking
 {
+    protected $emails = [];
+
+    /**
+     * @before
+     */
+    public function setUpMailTracking()
+    {
+        parent::setUp();
+
+        Mail::getSwiftMailer()
+            ->registerPlugin(new TestingMailEventListener($this));
+    }
+
+    public function addEmail(Swift_Message $email)
+    {
+        $this->emails[] = $email;
+    }
     
+    protected function seeEmailWasSent()
+    {
+        $this->assertNotEmpty($this->emails,
+            'No emails have been sent.');
+    }
+    
+    protected function seeEmailsSent($count)
+    {
+        $emailsSent = count($this->emails);
+
+        $this->assertCount($count, $this->emails,
+            "Expected $count emails to have been Sent, but $emailsSent were."
+        );
+    }
+}
+
+class TestingMailEventListener implements Swift_Events_EventListener
+{
+    protected $test;
+
+    public function __construct($test)
+    {
+        $this->test = $test;
+    }
+
+    public function beforeSendPerformed($event)
+    {
+        $this->test->addEmail($event->getMessage());
+    }
 }
