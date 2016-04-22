9c79939946c9fd17d3f6042637a9fe54afc8c461
lyndon <lyndon@moneyball.io>
2016-04-22 15:55:01 +0800

    Refractory ExampleTest.php move some feature to MailTracking

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,15 +6,9 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
-    protected $emails = [];
+    use MailTracking;
 
-    public function setUp()
-    {
-        parent::setUp();
-
-        Mail::getSwiftMailer()
-            ->registerPlugin(new TestingMailEventListener($this));
-    }
+   
     /**
      * A basic functional test example.
      *
@@ -27,41 +21,16 @@ class ExampleTest extends TestCase
             $message->from('foo@bar.com');
         });
 
+        Mail::raw('Hello world', function ($message){
+            $message->to('foo@bar.com');
+            $message->from('foo@bar.com');
+        });
+
 //        $this->seeEmailWasSent();
         $this->seeEmailsSent(2);
     }
 
-    public function addEmail(Swift_Message $email)
-    {
-        $this->emails[] = $email;
-    }
-
-    protected function seeEmailWasSent()
-    {
-        $this->assertNotEmpty($this->emails, 'No emails have been sent.');
-    }
-
-    protected function seeEmailsSent($count)
-    {
-        $emailsSent = count($this->emails);
-
-        $this->assertCount($count, $this->emails,
-            "Expected $count emails to have been Sent, but $emailsSent were."
-            );
-    }
-}
-
-class TestingMailEventListener implements Swift_Events_EventListener
-{
-    protected $test;
-
-    public function __construct($test)
-    {
-        $this->test = $test;
-    }
     
-    public function beforeSendPerformed($event)
-    {
-        $this->test->addEmail($event->getMessage());
-    }
 }
+
+
