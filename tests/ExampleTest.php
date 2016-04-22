f3d8e3a98ccc3450b343026ce760bd8ed8b43d63
lyndon <lyndon@moneyball.io>
2016-04-22 14:43:26 +0800

    Refractory testBasicExample in ExampleTest - video 12

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -13,15 +13,19 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-//        $this->visit('/')
-//             ->see('Laravel 5');
+
         //  1. Visit the home page.
         //  2. Press a  "Click Me" Link.
         //  3. See "You've been clicked, punk."
         //  4. Assert that the current url is /feedback.
+//        $this->visit('/')
+//            ->click('Click Me')
+//            ->see("You've been clicked, punk.")
+//            ->seePageIs('/feedback');
         $this->visit('/')
-            ->click('Click Me')
-            ->see("You've been clicked, punk.")
-            ->seePageIs('/feedback');
+            ->see('Laravel 5')
+            ->seeEmailWasSent();
     }
+
+   
 }
