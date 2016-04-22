3b80fcd3f6925bf1b91294d1069da381af4e9284
lyndon <lyndon@moneyball.io>
2016-04-22 14:06:36 +0800

    Fix Expression.php

--- src/Expression.php
+++ src/Expression.php
@@ -9,41 +9,54 @@ class Expression
         return new static;
     }
 
-    public function find($value)
+    protected function sanitize($value)
+    {
+        return $value = preg_quote($value, '/');
+    }
+
+    protected function add($value)
     {
         $this->expression .= $value;
 
         return $this;
     }
 
+    public function find($value)
+    {
+        return $this->add($this->sanitize($value));
+    }
+
     public function then($value)
     {
+
         return $this->find($value);
     }
 
     public function anything()
     {
-        $this->expression .= '.*';
-
-        return $this;
+        return $this->add('.*');
     }
 
     public function maybe($value)
     {
-        $value = preg_quote($value, '/');
+        $value =  $this->sanitize($value);
 
-        $this->expression .= '('.$value.')?';
-
-        return $this;
+        return $this->add("($value)?");
     }
 
     public function test($value)
     {
+//        var_dump($this->getRegex());
         return (bool) preg_match($this->__toString(), $value);
+    }
+
+    public function getRegex()
+    {
+        return '/' . $this->expression . '/';
     }
 
     public function __toString()
     {
-        return '/' . $this->expression . '/';
+        return $this->getRegex();
     }
 }
