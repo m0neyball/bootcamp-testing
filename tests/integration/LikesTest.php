10073cd11e39e65452a0d788ecc0a5089a746313
ben <ben@moneyball.io>
2016-04-22 13:14:35 +0800

    Test a user can like a post for LikesTest

--- tests/integration/LikesTest.php
+++ tests/integration/LikesTest.php
@@ -1,6 +1,27 @@
 <?php
+use App\User;
+use App\Post;
 
 class LikesTest extends TestCase
 {
-
+    /**
+     * @test
+     */
+    public function a_user_can_like_a_post ()
+    {
+        // given I have a post
+        $post = factory (Post::class)->create ();
+        // and a user
+        $user = factory (User::class)->create ();
+        // and that user is logged in
+        $this->actingAs ($user);
+        // when they like a post
+        $post->like ();
+        // then we should see evidence in the database, and the post should be liked.
+        $this->seeInDatabase ('likes', [
+            'user_id'       => $user->id,
+            'likeable_id'   => $post->id,
+            'likeable_type' => get_class ($post),
+        ]);
+    }
 }
