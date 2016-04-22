6c94d11dd94a9dcc222364ca69d3e4fe13c47697
lyndon <lyndon@moneyball.io>
2016-04-22 17:40:03 +0800

    Fix BladeDirective class

--- src/BladeDirective.php
+++ src/BladeDirective.php
@@ -8,8 +8,15 @@
 
 class BladeDirective
 {
+    protected $cache;
+    
     public function __construct(RussianCache $cache)
     {
-        
+        $this->cache = $cache;
+    }
+
+    public function setUp($key)
+    {
+        $this->cache->has($key);
     }
 }
