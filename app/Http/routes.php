1f57b5adf3a864dc8d2515dfde09789a38e5ef74
lyndon <lyndon@moneyball.io>
2016-04-22 17:01:17 +0800

    Fix route::get(/) in routes.php - video 12

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,12 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    Mail::raw('Hello world', function ($message){
+        $message->to('foo@bar.com');
+        $message->from('bar@foo.com');
+    });
+
+    return 'Email was sent';
 });
 
 Route::get('feedback', function () {
