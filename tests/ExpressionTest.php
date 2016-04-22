fc241660633966d36fa40c71bc8dccb0ef3ea1a2
lyndon <lyndon@moneyball.io>
2016-04-22 10:23:45 +0800

    Add ExpressionTest

---
+++ tests/ExpressionTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ExpressionTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_finds_a_string()
+    {
+
+    }
+}
