8f174650775b99707d7487328e542840af7208bd
ben <ben@moneyball.io>
2016-04-22 19:20:45 +0800

    Add Expression class make & find & then method

--- src/Expression.php
+++ src/Expression.php
@@ -2,5 +2,18 @@
 
 class Expression
 {
+    public static function make ()
+    {
+        return new static;
+    }
 
+    public function find ($value)
+    {
+        return '/' . $value . '/';
+    }
+
+    public function then ($value)
+    {
+        return $this->find($value);
+    }
 }
