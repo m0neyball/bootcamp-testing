027b82619bc360edef95a0b39147c9c078b46bb2
lyndon <lyndon@moneyball.io>
2016-04-22 17:05:25 +0800

    Add ProphecyExampleTest - video 13

---
+++ tests/ProphecyExampleTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 2016/4/22
+ * Time: 下午5:03
+ */
+class ProphecyExampleTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function test_something()
+    {
+        $this->prophesize();
+    }
+}
