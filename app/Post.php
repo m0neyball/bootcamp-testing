73d00052304a99d08d130608342e4446a6880db6
ben <ben@moneyball.io>
2016-04-22 17:18:46 +0800

    Refractory Post model file, use Likeability trait

--- app/Post.php
+++ app/Post.php
@@ -2,50 +2,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Auth;
 
 class Post extends Model
 {
-    public function like ()
-    {
-        $like = new Like([
-            'user_id' => Auth::id ()
-        ]);
-        $this->likes ()->save ($like);
-    }
-
-    public function unlike ()
-    {
-        $this
-            ->likes ()
-            ->where ('user_id', Auth::id ())
-            ->delete ();
-    }
-
-    public function toggle ()
-    {
-        if ($this->isLiked ()) {
-            $this->unlike ();
-        } else {
-            $this->like ();
-        }
-    }
-
-    public function isLiked ()
-    {
-        return !!$this
-            ->likes ()
-            ->where ('user_id', Auth::id ())
-            ->count ();
-    }
-
-    public function getLikesCountAttribute ()
-    {
-        return $this->likes()->count();
-    }
-
-    public function likes ()
-    {
-        return $this->morphMany ('App\Like', 'likeable');
-    }
+    use Likeability;
 }
