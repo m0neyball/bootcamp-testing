bfe4b33f81013e47685b885e77d19ced4597564d
ben <ben@moneyball.io>
2016-04-22 14:10:11 +0800

    Test a user can unlike a post for LikesTest

--- tests/integration/LikesTest.php
+++ tests/integration/LikesTest.php
@@ -25,4 +25,28 @@ class LikesTest extends TestCase
         ]);
         $this->assertTrue ($post->isLiked ());
     }
+
+    /**
+     * @test
+     */
+    public function a_user_can_unlike_a_post ()
+    {
+        // given I have a post
+        $post = factory (Post::class)->create ();
+        // and a user
+        $user = factory (User::class)->create ();
+        // and that user is logged in
+        $this->actingAs ($user);
+        // when they like a post
+        $post->like ();
+        // when they unlike a post
+        $post->unlike ();
+        // then we should see evidence in the database, and the post should be liked.
+        $this->notSeeInDatabase ('likes', [
+            'user_id'       => $user->id,
+            'likeable_id'   => $post->id,
+            'likeable_type' => get_class ($post),
+        ]);
+        $this->assertFalse ($post->isLiked ());
+    }
 }
