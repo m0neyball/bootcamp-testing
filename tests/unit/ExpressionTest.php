3042b9df0d42576688f5b33a4fc5edab9d6f7dab
fox <fox@moneyball.io>
2016-04-24 17:10:55 +0800

    Test expression make

---
+++ tests/unit/ExpressionTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Expression;
+
+class ExpressionTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test it finds a string
+     */
+    public function it_finds_a_string()
+    {
+        $regex = Expression::make()->find('www');
+        $this->assertRegExp($regex, 'www');
+
+        $regex = Expression::make()->then('www');
+        $this->assertRegExp($regex, 'www');
+    }
+
+    /**
+     * @test it checks for anything
+     */
+    public function it_checks_for_anything()
+    {
+        $regex = Expression::make()->anything();
+
+        $this->assertRegExp($regex, 'foo');
+    }
+
+    /**
+     * @test it maybe has a value
+     */
+    public function it_maybe_has_a_value()
+    {
+        $regex = Expression::make()->maybe('http');
+
+        $this->assertRegExp($regex, 'http');
+        $this->assertRegExp($regex, '');
+    }
+}
