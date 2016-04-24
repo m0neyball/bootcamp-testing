30f105c854fd3d93d189e1220389ea6abf3fa3b5
fox <fox@moneyball.io>
2016-04-25 03:16:29 +0800

    Add model ide helper annotation - video12

--- app/Like.php
+++ app/Like.php
@@ -4,6 +4,21 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Like
+ *
+ * @property integer $user_id
+ * @property integer $likeable_id
+ * @property string $likeable_type
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\App\Like whereUserId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Like whereLikeableId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Like whereLikeableType($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Like whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Like whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Like extends Model
 {
     protected $fillable = ['user_id'];
