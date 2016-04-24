df270e90fbcd808f5cff1f97750cd9c4e562b128
fox <fox@moneyball.io>
2016-04-25 04:08:43 +0800

    Test it normalizes an array for the cache key

--- tests/unit/DemoTest.php
+++ tests/unit/DemoTest.php
@@ -17,6 +17,9 @@ class DemoTest extends PHPUnit_Framework_TestCase
         $directive->setUp('cache-key');
     }
 
+    /**
+     * @test it normalizes a cacheable model for the cache key
+     */
     public function it_normalizes_a_cacheable_model_for_the_cache_key()
     {
         $cache = $this->prophesize(RussianCache::class);
@@ -25,6 +28,21 @@ class DemoTest extends PHPUnit_Framework_TestCase
         $cache->has('stub-cache-key')->shouldBeCalled();
 
         $directive->setUp(new ModelStub);
+
+    }
+
+    /**
+     * @test it normalizes an array for the cache key
+     */
+    public function it_normalizes_an_array_for_the_cache_key()
+    {
+        $cache = $this->prophesize(RussianCache::class);
+        $directive = new BladeDirective($cache->reveal());
+
+        $item = ['foo', 'bar'];
+        $cache->has(md5('foobar'))->shouldBeCalled();
+
+        $directive->setUp($item);
 
     }
 }
