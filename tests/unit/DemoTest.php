c4c483ffd04ab2c7cf02c07c8e9bcfa1ee9a4dab
fox <fox@moneyball.io>
2016-04-25 03:59:20 +0800

    Test it normalizes a cacheable model for the cache key

--- tests/unit/DemoTest.php
+++ tests/unit/DemoTest.php
@@ -14,7 +14,25 @@ class DemoTest extends PHPUnit_Framework_TestCase
         $directive = new BladeDirective($cache->reveal());
 
         $cache->has('cache-key')->shouldBeCalled();
-
         $directive->setUp('cache-key');
+    }
+
+    public function it_normalizes_a_cacheable_model_for_the_cache_key()
+    {
+        $cache = $this->prophesize(RussianCache::class);
+        $directive = new BladeDirective($cache->reveal());
+
+        $cache->has('stub-cache-key')->shouldBeCalled();
+
+        $directive->setUp(new ModelStub);
+
+    }
+}
+
+class ModelStub
+{
+    public function getCacheKey()
+    {
+        return 'stub-cache-key';
     }
 }
