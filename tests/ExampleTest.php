02bb73c64aff96f106b98d5f785401c555bf1ec8
ben <ben@moneyball.io>
2016-04-24 11:16:14 +0800

    Test testBasicEmailExample for ExampleTest

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -5,27 +5,59 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+    protected $emails = [];
+
+    public function setUp ()
+    {
+        parent::setUp ();
+        Mail::getSwiftMailer ()
+            ->registerPlugin (new TestingMailEventListener($this));
+    }
+
     /**
      * A basic functional test example.
      *
      * @return void
      */
-    public function testBasicExample ()
+    public function testBasicEmailExample ()
     {
-        // 1. Visit the home page
-        // 2. Press a "Click Me" link
-        // 3. See "You're been clicked, punk."
-        // 4. Assert that the current url /feedback
-        /*
-        $this->visit('/');
-        $this->click('Click Me');
-        $this->see("You're been clicked, punk.");
-        $this->seePageIs('/feedback');
-        */
-        $this
-            ->visit ('/')
-            ->click ('Click Me')
-            ->see ("You're been clicked, punk.")
-            ->seePageIs ('/feedback');
+        Mail::raw ('Hello World', function ($message) {
+            $message->to ('foo@bar.com');
+            $message->from ('bar@foo.com');
+        });
+        $this->seeEmailWasSent ();
+    }
+
+    protected function seeEmailWasSent ()
+    {
+        $this->assertNotEmpty (
+            $this->emails,
+            'No emails have been sent.'
+        );
+    }
+
+    public function addEmail (Swift_Message $email)
+    {
+        $this->emails[] = $email;
+    }
+}
+
+class TestingMailEventListener implements Swift_Events_EventListener
+{
+
+    /**
+     * @var
+     */
+    private $test;
+
+    public function __construct ($test)
+    {
+        $this->test = $test;
+    }
+
+    public function beforeSendPerformed ($event)
+    {
+        $message = $event->getMessage ();
+        $this->test->addEmail ($message);
     }
 }
