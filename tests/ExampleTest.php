6a9d6e947b099b98488579c1e6eb899d5613bda5
ben <ben@moneyball.io>
2016-04-24 11:37:51 +0800

    Refractory testBasicEmailExample use MailTracking

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -5,14 +5,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
-    protected $emails = [];
-
-    public function setUp ()
-    {
-        parent::setUp ();
-        Mail::getSwiftMailer ()
-            ->registerPlugin (new TestingMailEventListener($this));
-    }
+    use MailTracking;
 
     /**
      * A basic functional test example.
@@ -30,49 +23,7 @@ class ExampleTest extends TestCase
             $message->from ('bar@foo.com');
         });
         $this->seeEmailWasSent ();
-        $this->seeEmailsSent(2);
-    }
-
-    protected function seeEmailWasSent ()
-    {
-        $this->assertNotEmpty (
-            $this->emails,
-            'No emails have been sent.'
-        );
-    }
-
-    public function seeEmailsSent ($count)
-    {
-        $emailSent = count($this->emails);
-        $this->assertCount(
-            $count,
-            $this->emails,
-            "Expected $count emails to have been sent, but $emailSent were."
-        );
-    }
-
-    public function addEmail (Swift_Message $email)
-    {
-        $this->emails[] = $email;
+        $this->seeEmailsSent (2);
     }
 }
 
-class TestingMailEventListener implements Swift_Events_EventListener
-{
-
-    /**
-     * @var
-     */
-    private $test;
-
-    public function __construct ($test)
-    {
-        $this->test = $test;
-    }
-
-    public function beforeSendPerformed ($event)
-    {
-        $message = $event->getMessage ();
-        $this->test->addEmail ($message);
-    }
-}
---
+++ tests/MailTracking.php
@@ -0,0 +1,59 @@
+<?php
+
+trait MailTracking
+{
+    protected $emails = [];
+
+    /**
+     * @before
+     */
+    public function setUpMailTracking ()
+    {
+        parent::setUp ();
+        Mail::getSwiftMailer ()
+            ->registerPlugin (new TestingMailEventListener($this));
+    }
+
+    protected function seeEmailWasSent ()
+    {
+        $this->assertNotEmpty (
+            $this->emails,
+            'No emails have been sent.'
+        );
+    }
+
+    public function seeEmailsSent ($count)
+    {
+        $emailSent = count ($this->emails);
+        $this->assertCount (
+            $count,
+            $this->emails,
+            "Expected $count emails to have been sent, but $emailSent were."
+        );
+    }
+
+    public function addEmail (Swift_Message $email)
+    {
+        $this->emails[] = $email;
+    }
+}
+
+class TestingMailEventListener implements Swift_Events_EventListener
+{
+
+    /**
+     * @var
+     */
+    private $test;
+
+    public function __construct ($test)
+    {
+        $this->test = $test;
+    }
+
+    public function beforeSendPerformed ($event)
+    {
+        $message = $event->getMessage ();
+        $this->test->addEmail ($message);
+    }
+}
